43ed7a4bc712a4b9614740dfc883a931008b8439
NURDIANSYAH <nudiansyahdian28.adv@gmail.com>
2026-09-20 12:27:19 +0700

    chore(cleanup): remove obsolete patch scripts and scratch files

--- patch_index2.py
+++
@@ -1,7 +0,0 @@
-with open("index.html", "r") as f:
-    content = f.read()
-
-content = content.replace("</head>", '  <link rel="canonical" href="https://nurdiansyahlabs.com" />\n</head>')
-
-with open("index.html", "w") as f:
-    f.write(content)
